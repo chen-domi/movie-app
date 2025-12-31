25cffb0b5d793e4760dc5bb06e853975f5bd12d2
Dominic Chen <cchendominic@gmail.com>
2025-12-31 08:53:20 -0500

    Add Tailwind, images, and Search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Search from './components/Search.jsx'
+import { useState } from 'react'
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+
   return (
-    <div>App</div>
+    <main>
+      <div className="pattern"/>
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero-img.png" alt="Hero Banner" />
+          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without The Hassle</h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+    </main>
   )
 }
 
